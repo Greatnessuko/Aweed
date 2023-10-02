4dd3ff0de2e4cc70172ae19fae5cbf107c3f4712
Greatnessuko <greatnessuko@gmail.com>
2023-10-02 16:04:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/hair.ts
@@ -0,0 +1,47 @@
+import guy1 from "./assets/guys1.webp";
+import guy2 from "./assets/guy2.webp";
+import guy3 from "./assets/guy3.webp";
+import guy4 from "./assets/guy4.webp";
+import guy5 from "./assets/guy5.webp";
+import guy6 from "./assets/guy6.webp";
+
+const hair = [
+	{
+		name: "mysterious figure",
+		image: guy1,
+		data:
+			"A mysterious figure emerges from the shadows, wielding incredible powers and a dark secret.",
+	},
+	{
+		name: "devastated by war",
+		image: guy2,
+		data:
+			"In a world devastated by war, a group of unlikely heroes must band together to save humanity.",
+	},
+	{
+		name: "hidden portal",
+		image: guy3,
+		data:
+			"A young girl discovers a hidden portal that leads to a magical realm filled with fantastical creatures.",
+	},
+	{
+		name: "brilliant scientist",
+		image: guy4,
+		data:
+			"A brilliant scientist invents a time machine and must navigate the complexities of altering the past to save the future.",
+	},
+	{
+		name: "epic adventure",
+		image: guy5,
+		data:
+			"A group of friends embark on an epic adventure across uncharted lands, facing danger and discovering hidden treasures.",
+	},
+	{
+		name: "struggling artist",
+		image: guy6,
+		data:
+			"A struggling artist finds inspiration in the most unexpected places and creates a masterpiece that changes the art world forever.",
+	},
+];
+
+export default hair;
